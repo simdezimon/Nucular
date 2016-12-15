1d7a6856ddde10479140e0c7026afabdddd8e844
Yousei9 <yousei.kyoko+github@gmail.com>
2016-12-15 18:24:57 +0100

    enhanced reactor fluid boxes
    - added fluid boxes to all corners
    - turn off fluid connectors for breeder

--- Nucular/prototypes/reactor-entity.lua
+++ Nucular/prototypes/reactor-entity.lua
@@ -77,14 +77,28 @@ data:extend(
           pipe_covers = pipecoverspictures(),
           base_area = 10,
           base_level = -1,
-          pipe_connections = {{ type="input", position = {-2, 1} },{ type="input", position = {-1, 2} }}
+          pipe_connections = {{ type="input", position = {-1, -2} }}
+        },
+        {
+          production_type = "input",
+          pipe_covers = pipecoverspictures(),
+          base_area = 10,
+          base_level = -1,
+          pipe_connections = {{ type="input", position = {1, -2} }}
         },
         {
           production_type = "output",
           pipe_covers = pipecoverspictures(),
           base_level = 1,
-          pipe_connections = {{ position = {2, 1} },{ position = {1, 2}  }}
-        }
+          pipe_connections = {{ position = {-1, 2} }}
+        },
+        {
+          production_type = "output",
+          pipe_covers = pipecoverspictures(),
+          base_level = 1,
+          pipe_connections = {{ position = {1, 2} }}
+        },
+      off_when_no_fluid_recipe = true
       }
     }
   })
